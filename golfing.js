8a6176a87710e473c1bb7d9a315a2e074832c87f
lebbe <lebchen@gmail.com>
2014-05-02 15:48:17 +0200

    Adding golfing file.

---
+++ golfing.js
@@ -0,0 +1,23 @@
+
+/**
+ * Feel free to pull-request your own golfing efforts.
+ * 
+ * This file contains of two sections:
+ *
+ * Section 1: A bunch of variables and functions declared to minimzalize code. For instance (sub)strings used several times in section two.
+ * The variables are inserted to the closed scope within the iffy by initializing them on the instantiation of the iffy.
+ * The first variable is named a, second b, and so on. If we reach z, next one will be named aa, then ab, etc.
+ *
+ * Section 2: The feature detection functions.
+ *
+ * This file is not copyrighted, and belongs to the public domain.
+ */
+// Section 1:
+ (function(a) {
+
+// Section 2:
+HAZ
+	.add(a,function(t){var e=t.c(a),g='getContext';return(e[g]&&eg);});
+
+// Section 1:
+})('canvas');
